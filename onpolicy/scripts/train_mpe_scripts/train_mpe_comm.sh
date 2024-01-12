57d9ce5f48c7f5a0b9336b4528f0a72efd51d426
Armin Saghafian <armin.saghafian@gmail.com>
2024-01-12 20:41:57 +0330

    added eval

--- onpolicy/scripts/train_mpe_scripts/train_mpe_comm.sh
+++ onpolicy/scripts/train_mpe_scripts/train_mpe_comm.sh
@@ -14,5 +14,5 @@ do
     CUDA_VISIBLE_DEVICES=0 python ../train/train_mpe.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
     --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} \
     --n_training_threads 1 --n_rollout_threads 1 --num_mini_batch 1 --episode_length 25 --num_env_steps 10000 \
-    --ppo_epoch 15 --gain 0.01 --lr 7e-4 --critic_lr 7e-4 --user_name "yuchao" --share_policy
+    --ppo_epoch 15 --gain 0.01 --lr 7e-4 --critic_lr 7e-4 --user_name "yuchao" --share_policy --use_eval --eval_interval 2
 done
