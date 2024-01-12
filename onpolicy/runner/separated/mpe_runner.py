6b6fbb0eb87c59632a30f5b94fe517cf5a339388
Armin Saghafian <armin.saghafian@gmail.com>
2024-01-12 21:56:33 +0330

    debug

--- onpolicy/runner/separated/mpe_runner.py
+++ onpolicy/runner/separated/mpe_runner.py
@@ -310,4 +310,4 @@ class MPERunner(Runner):
                 print("eval average episode rewards of agent%i: " % agent_id + str(average_episode_rewards))
         
         if self.all_args.save_gifs:
-            imageio.mimsave(str(self.gif_dir) + '/render.gif', all_frames, duration=self.all_args.ifi)
+            imageio.mimsave('./render.gif', all_frames, duration=self.all_args.ifi)
