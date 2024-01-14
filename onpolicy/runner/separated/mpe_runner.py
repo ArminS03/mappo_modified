cdaf07517e41c3a3fbb93cc27e05ba2f3d0baec8
Armin Saghafian <armin.saghafian@gmail.com>
2024-01-14 20:49:33 +0330

    debug

--- onpolicy/runner/separated/mpe_runner.py
+++ onpolicy/runner/separated/mpe_runner.py
@@ -309,8 +309,8 @@ class MPERunner(Runner):
             for agent_id in range(self.num_agents):
                 average_episode_rewards = np.mean(np.sum(episode_rewards[:, :, agent_id], axis=0))
                 print("eval average episode rewards of agent%i: " % agent_id + str(average_episode_rewards))
-        print("saving ...")
         print(len(all_frames))
         print(f'ifi: {self.all_args.ifi}')
         if self.all_args.save_gifs:
+            print("saving ...")
             imageio.mimsave('./render.gif', all_frames, duration=self.all_args.ifi)
